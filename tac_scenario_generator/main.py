c85c24074e0fad2146c29fca452e480b29e1e623
fildred13 <fildred13@gmail.com>
2023-07-23 00:01:46 -0400

    first draft poc

--- tac_scenario_generator/main.py
+++ tac_scenario_generator/main.py
@@ -1,7 +1,39 @@
-def add_numbers(a, b):
-    return a + b
+import time
+
+import pytesseract
+import pyautogui
+
+def capture_screen():
+    # Capture the entire screen and return the screenshot image.
+    screenshot = pyautogui.screenshot()
+    return screenshot
+
+
+def extract_text_from_image(image):
+    # TODO: Implement OCR to extract text from the image and return the result.
+    pass
+
+
+def get_coordinates_for_text(text, image):
+    # TODO: Implement function to find the coordinates of the provided text.
+    pass
+
+
+def find_and_click_target_text(target_text):
+    # The script currently assumes that the user will navigate to the game
+    # window after initializing the script, to simplify development. We'll want
+    # to replace this with something which automatically maximizes the game in
+    # the future. This sleep provides the user time to do that.
+    time.sleep(3)
+
+    screenshot = capture_screen()
+    #ocr_result = extract_text_from_image(screenshot)
+    #x, y = get_coordinates_from_text(target_text, screenshot):
+
+    # Send a mouse click on the target text's position
+    #pyautogui.click(x, y)
 
 
 if __name__ == "__main__":
-    result = add_numbers(2, 3)
-    print(f"Result: {result}")
+    target_text = "OK"
+    find_and_click_target_text(target_text)
